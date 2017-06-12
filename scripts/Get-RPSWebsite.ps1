806708ae17b934773d69e0bc5b7ef8ab2a320a49
Masamoto Miyata <miyata@sincere-co.com>
2017-06-12 19:40:04 +0900

    Removed the Name argument because it is redundant.

--- scripts/Get-RPSWebsite.ps1
+++ scripts/Get-RPSWebsite.ps1
@@ -2,24 +2,13 @@ function Get-RPSWebsite{
   [CmdletBinding()]
   param (
     [parameter(Mandatory=$true,ValueFromPipeline=$true)]
-    [RPS.Server]$Server,
-
-    [parameter(Mandatory=$false,Position=0)]
-    [string]$Name
+    [RPS.Server]$Server
   )
   PROCESS {
     $cred = Create-RPSCredential $Server
     Invoke-Command -ComputerName $Server.Address -Credential $cred -ScriptBlock {
-      $Name = $args[0]
-
-      $command = "Get-Website"
-
-      if($Name -ne ""){
-        $command += " -Name ""$Name"""
-      }
-
-      Invoke-Expression -Command $command
-    } -argumentlist $Name | %{
+      Get-Website
+    }  | %{
       return new-object RPS.Website $Server, $_, $_.name
     }
   }
